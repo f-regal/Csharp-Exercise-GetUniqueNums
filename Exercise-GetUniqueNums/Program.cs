114e74b03cdbedf48ed4bb023130fc5e32d68688
Faysal Regal <faysalregal@gmail.com>
2022-03-20 16:07:24 +0000

    * .gitignore: * Exercise-GetUniqueNums.sln: * Program.cs: * AssemblyInfo.cs: * Exercise-GetUniqueNums.csproj.user:
    * Exercise-GetUniqueNums.csproj: firstCommit

---
+++ Exercise-GetUniqueNums/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExerciseGetUniqueNums
+{
+    class MainClass
+    {
+        //Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+        //The list of numbers may include duplicates. Display the unique numbers that the user has entered.
+
+        public static void Main(string[] args)
+        {
+
+            var quit = false;
+            List<int> numList = new List<int>();
+            List<int> noDupes = new List<int>();
+
+            while (quit == false)
+            {
+                Console.WriteLine("Please Enter Any Number Or Type 'Quit' to Exit");
+                var userInput = Console.ReadLine();
+                
+
+
+                if (userInput == "Quit")
+                {
+                    quit = true;
+                } else
+                {
+                    var numInput = Convert.ToInt32(userInput);
+                    numList.Add(numInput);
+                    continue;
+                }
+            }
+
+            
+            for (var i = 0; i < numList.Count; i++)
+            {
+                if (noDupes.Contains(numList[i]))
+                {
+                    continue;
+                } else
+                {
+                    noDupes.Add(numList[i]);
+                }
+            }
+
+            foreach (var num in noDupes)
+            {
+                Console.WriteLine("Unique Number: " + num);
+            }
+        }
+    }
+}
